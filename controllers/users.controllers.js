49d8c8201a0e290c7c0ad931b0e7143b25fc77c2
Hard-Luck <tyreemarka@gmail.com>
2022-09-05 15:13:35 +0100

    fixed test and removed console.log

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,7 +1,6 @@
 const { getAllUsers } = require("../models/users.models");
 
 exports.getUsers = (req, res, next) => {
-  console.log("in controller");
   getAllUsers()
     .then((users) => {
       res.status(200).send({ users });

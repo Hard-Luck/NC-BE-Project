5a59d3aecbda64683881496b99a50dde806b9def
Hard-Luck <tyreemarka@gmail.com>
2022-09-05 13:16:36 +0100

    Implemented get-reviews-by-id with testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,7 +8,7 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end);
 
 describe("NC_Games API", () => {
-  describe("GET Categories ", () => {
+  describe("GET /api/Categories", () => {
     it("200: Returns all category objects with slug and description", () => {
       return request(app)
         .get("/api/categories")
@@ -19,6 +19,42 @@ describe("NC_Games API", () => {
             expect(category).toHaveProperty("slug", expect.any(String));
             expect(category).toHaveProperty("description", expect.any(String));
           });
+        });
+    });
+  });
+  describe("GET /api/reviews/:review_id", () => {
+    it("200: Returns all reviews for the given review_id", () => {
+      return request(app)
+        .get("/api/reviews/2")
+        .expect(200)
+        .then(({ body }) => {
+          const { review } = body;
+          expect(review).toHaveProperty("review_id", expect.any(Number));
+          expect(review).toHaveProperty("title", expect.any(String));
+          expect(review).toHaveProperty("review_body", expect.any(String));
+          expect(review).toHaveProperty("designer", expect.any(String));
+          expect(review).toHaveProperty("review_img_url", expect.any(String));
+          expect(review).toHaveProperty("votes", expect.any(Number));
+          expect(review).toHaveProperty("category", expect.any(String));
+          expect(review).toHaveProperty("owner", expect.any(String));
+          expect(review).toHaveProperty("created_at", expect.any(String));
+          expect(Date.parse(review.created_at)).not.toBeNaN();
+        });
+    });
+    it("400: bad request, when review id is valid but doesn't exist", () => {
+      return request(app)
+        .get("/api/reviews/10000000")
+        .expect(400)
+        .then(({ body }) => {
+          expect(body).toEqual({ msg: "review not found" });
+        });
+    });
+    it("400: bad request, when review id is valid but doesn't exist", () => {
+      return request(app)
+        .get("/api/reviews/invalid")
+        .expect(400)
+        .then(({ body }) => {
+          expect(body).toEqual({ msg: "bad request" });
         });
     });
   });
--- models/categories.models.js
+++ models/categories.models.js
@@ -1,6 +1,18 @@
 const db = require("../db/connection");
+
 exports.getAllCategories = () => {
   return db.query(`SELECT * FROM categories`).then((response) => {
     return response.rows;
   });
+};
+
+exports.getReviewById = (review_id) => {
+  return db
+    .query(`SELECT * FROM reviews WHERE review_id = $1`, [review_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 400, msg: "review not found" });
+      }
+      return rows[0];
+    });
 };

fab9cb4882ff35d87b164ee412eaf873e073d54c
Hard-Luck <tyreemarka@gmail.com>
2022-09-09 13:47:27 +0100

    tiny refactor

--- models/users.models.js
+++ models/users.models.js
@@ -1,17 +1,14 @@
 const db = require("../db/connection");
-
+const { selectAllFromTableWhere } = require("./utils");
 exports.getAllUsers = async () => {
   const { rows } = await db.query("SELECT * FROM users");
   return rows;
 };
 
 exports.getUserByUsername = async (username) => {
-  const { rowCount, rows } = await db.query(
-    "SELECT * FROM users where username = $1",
-    [username]
-  );
-  if (rowCount === 0) {
+  const user = await selectAllFromTableWhere(db, "users", "username", username);
+  if (user.length === 0) {
     return Promise.reject({ status: 404, msg: "user not found" });
   }
-  return rows[0];
+  return user[0];
 };
